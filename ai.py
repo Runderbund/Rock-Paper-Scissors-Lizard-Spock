8e2935b82df3af31420cac89455b670a8a56c023
unknown <sean.herbison@gmail.com>
2023-02-16 11:36:26 -0700

    Starts Game class and adds gesture choice framework for players

--- ai.py
+++ ai.py
@@ -5,3 +5,6 @@ class AI(Player):
     def __init__(self, name):
         super().__init__()
         self.name = name
+    
+    def choose_gesture(self):
+        self.gesture = random.randint(0, 4)
--- human.py
+++ human.py
@@ -4,3 +4,6 @@ class Human(Player):
     def __init__(self, name):
         super().__init__()
         self.name = name
+
+    def choose_gesture(self):
+        self.gesture = input("Please choose a gesture (1-4): ")
