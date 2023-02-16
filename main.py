602009105a3e11a9f0ff01275d88f452cb0c10be
unknown <sean.herbison@gmail.com>
2023-02-15 20:57:48 -0700

    Pushes initial test commit. Just main and some comments

---
+++ main.py
@@ -0,0 +1,11 @@
+# 10 Commits
+# Use inheritance
+# Check user input, re-ask
+    # Use @ on these?
+# Store choices as List of Strings
+# Correctly choose winner
+# Game is best of 3
+    # Could give options as extra
+# Have option for single (human vs PC) or multiplayer (human vs human)
+    # I was assuming more than one player. Doesn't sound like it.
+        # Maybe give option. Would complicate it.
